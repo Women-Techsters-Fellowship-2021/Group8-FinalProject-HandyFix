dd29bb6921625332c2029fb27507c18aa10b00fc
Ogundipe Abimbola <ogundipeabimbola9@gmail.com>
2021-08-22 08:47:31 +0100

    commit login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,13 +1,35 @@
-
 function Login() {
-    return (
-    
-          <p>
-           Hi people! Let get started.
+  return (
+    <div className="App">
+      <div className="Loginh1">
+        <h1>HandyFix </h1>
+      </div>
+      <div>
+        <h2 className="Loginh2">Login</h2>
+      </div>
+      <div className="Loginform">
+        <input
+          className="Login"
+          placeholder="Email Address"
+          type="text"
+        ></input>
+        <input className="Login" placeholder="Password" type="text"></input>
+        <button className="Loginbtn" type="submit">
+          Continue
+        </button>
+        <p className="LoginP">OR</p>
+        <button className="Loginbtn" type="submit">
+          Sign in with google
+        </button>
+      </div>
+      <div className="forgetpassword">
+        <p>
+          <b>forget password?</b>
+        </p>
+        <p>sign in</p>
+      </div>
+    </div>
+  );
+}
 
-          </p>
-    )}
-         
-   
-  
-  export default Login;
+export default Login;
