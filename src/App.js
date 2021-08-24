bafe06d4be9185551d95e3157d08e005a26acd4e
Ogundipe Abimbola <ogundipeabimbola9@gmail.com>
2021-08-24 15:10:24 +0100

    commit this

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-
 import './App.css';
-import Login from './Components/Login';
-import SignUp from './Components/Sign';
 
 function App() {
   return (
   <div>
-         <Login/> 
-        <SignUp/>
+        
         </div>
   )}
        
