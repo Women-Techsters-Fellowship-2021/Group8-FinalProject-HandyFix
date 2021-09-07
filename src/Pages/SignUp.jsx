a2d22b921a8da743b0132a52f614c8b412550b3c
Ogundipe Abimbola <ogundipeabimbola9@gmail.com>
2021-09-07 17:05:55 +0100

    signup

---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,54 @@
+import React from "react"
+// import background from ;
+
+function SignUp() {
+  return (
+    <div className="Container">
+        <div className="SignUp">
+        <h2>Sign Up</h2>
+        <div className="SignUpHead">
+          <h3>Welcome to HandyFix</h3>
+        </div>
+        <div className="SignUpForm">
+          <input
+            className="SignUpInput"
+            placeholder="Full Name"
+            type="text"
+          ></input>
+          <input
+            className="SignUpInput"
+            placeholder="Location"
+            type="text"
+          ></input>
+          <input
+            className="SignUpInput"
+            placeholder="Emial Address"
+            type="text"
+          ></input>
+          <input
+            className="SignUpInput"
+            placeholder="Password"
+            type="text"
+          ></input>
+          <input
+            className="SignUpInput"
+            placeholder="Confirm Password"
+            type="text"
+          ></input>
+        </div>
+        <div className="TandC">
+          <input type="checkbox" className="SignUpcheckbox" required></input>
+          <a href="">I agree to the term and condition of this project</a>
+        </div>
+        <div className="SignUpCtn">
+          <button type="submit" className="SignUpCtn">
+            Continue
+          </button>
+        </div>
+  </div>
+  <div className="Diagram"><p>hgvhggjhgvjhftgdgcfg</p></div>
+    </div>
+      
+  );
+}
+export default SignUp;
