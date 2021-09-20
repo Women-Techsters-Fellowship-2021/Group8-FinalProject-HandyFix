78a7ffa0e1b4f27018db784f7de42b68fdee6362
Awura-Precious <57904360+Awura-Precious@users.noreply.github.com>
2021-09-20 07:31:53 -0700

    retrieved navbar css,profile css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
 import './App.css';
 import LandingPage from './Pages/Landing';
 import { BrowserRouter } from 'react-router-dom';
+// import { useMediaQuery } from 'react-responsive';
+// import MediaQuery from 'react-responsive';
+
+
 // Switch, Route
 function App() {
+
+  //
+  // const isMobileDevice = useMediaQuery({
+  //   query: "(min-device-width: 480px)",
+  // });
+
+  // const isTabletDevice = useMediaQuery({
+  //   query: "(min-device-width: 768px)",
+  // });
+
+  // const isLaptop = useMediaQuery({
+  //   query: "(min-device-width: 1024px)",
+  // });
+
+  // const isDesktop = useMediaQuery({
+  //   query: "(min-device-width: 1200px)",
+  // });
+
+  // const isBigScreen = useMediaQuery({
+  //   query: "(min-device-width: 1201px )",
+  // });
+
+
   return (
     <div>
       <BrowserRouter>
